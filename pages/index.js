8dd8d63144934497084d4059be4db701025bfd4a
arye321 <arye321@users.noreply.github.com>
2023-04-07 18:05:21 +0000

    init

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+import Head from "next/head";
+
+export default function Home() {
+  const [socket_state, setSocket_state] = useState("connecting...");
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    fetch("api/socket");
+    setSocket(io())
+
+  }, []);
+  if (socket) {
+    socket.on('connect', () => {
+      console.log('connected', socket.id)
+    })
+  }
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Socket test" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <h1>socket state: {socket_state}</h1>
+      </div>
+    </>
+  );
+}
